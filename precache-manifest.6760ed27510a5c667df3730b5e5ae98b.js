469012b71ce14b0cb23ac719f4db4ec52b6bc594
alzebud <alzebud@gmail.com>
2018-11-26 17:10:51 +0200

    Updates

---
+++ precache-manifest.6760ed27510a5c667df3730b5e5ae98b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "54d18b5c11d55ed2bd4c",
+    "url": "/contactmanager/static/js/main.54d18b5c.chunk.js"
+  },
+  {
+    "revision": "180e63d2b7e3815f6531",
+    "url": "/contactmanager/static/js/1.180e63d2.chunk.js"
+  },
+  {
+    "revision": "54d18b5c11d55ed2bd4c",
+    "url": "/contactmanager/static/css/main.f5741332.chunk.css"
+  },
+  {
+    "revision": "180e63d2b7e3815f6531",
+    "url": "/contactmanager/static/css/1.3c56af5c.chunk.css"
+  },
+  {
+    "revision": "bba7d637745c93535c4bc31f7fbcec86",
+    "url": "/contactmanager/index.html"
+  }
+];
